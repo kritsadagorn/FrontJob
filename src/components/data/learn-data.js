e6a9743e15cf1232396b2f7fff1d970eae221b15
HANIBA <wuttipat2004@gmail.com>
2025-06-05 17:41:19 +0700

    MoreWebsite

--- src/components/data/learn-data.js
+++ src/components/data/learn-data.js
@@ -2,73 +2,209 @@ export const learnData = [
   {
     index: "freeCodeCamp",
     Website: "https://www.freecodecamp.org",
-    Description: "FreeCodeCamp Popular learning paths: Responsive Web Design (HTML & CSS), JavaScript, Front-End, Back-End, Data Analysis ",
+    Description:
+      "FreeCodeCamp Popular learning paths: Responsive Web Design (HTML & CSS), JavaScript, Front-End, Back-End, Data Analysis ",
     img: "https://yt3.googleusercontent.com/ytc/AIdro_lGRc-05M2OoE1ejQdxeFhyP7OkJg9h4Y-7CK_5je3QqFI=s900-c-k-c0x00ffffff-no-rj",
-    tags: ["Free", "Web Development", "Full Stack", "Certification"]
+    tags: ["Free", "Web Development", "Full Stack", "Certification"],
   },
   {
     index: "Codecademy",
     Website: "https://www.codecademy.com",
-    Description: "Codecademy is an online platform that teaches coding through interactive, hands-on lessons like: HTML, CSS, JavaScript, React, and responsive design, Python, Java, C++, C#, Swift, Go, SQL, Python for data analysis, machine learning basics, Algorithms, problem solving, object-oriented programming, Git, GitHub, Command Line, APIs, Testing",
+    Description:
+      "Codecademy is an online platform that teaches coding through interactive, hands-on lessons like: HTML, CSS, JavaScript, React, and responsive design, Python, Java, C++, C#, Swift, Go, SQL, Python for data analysis, machine learning basics, Algorithms, problem solving, object-oriented programming, Git, GitHub, Command Line, APIs, Testing",
     img: "https://yt3.googleusercontent.com/OoIfYjblzMBPeV3kAHvaOYNgQmvJeVUB9q14pRuAhawNFRppUstvr_inYiBzdVB6EuWhK4li=s900-c-k-c0x00ffffff-no-rj",
-    tags: ["Interactive", "Programming", "Web Development", "Mobile",]
+    tags: ["Interactive", "Programming", "Web Development", "Mobile"],
   },
   {
     index: "Khan Academy",
     Website: "https://www.khanacademy.org/computing/computer-programming",
-    Description: "Khan Academy offers free courses on computer programming, including JavaScript, HTML/CSS, and SQL basics, Drawing & Animation, Games & Visualizations, Querying and Managing Data,",
+    Description:
+      "Khan Academy offers free courses on computer programming, including JavaScript, HTML/CSS, and SQL basics, Drawing & Animation, Games & Visualizations, Querying and Managing Data,",
     img: "https://images.seeklogo.com/logo-png/39/1/khan-academy-logo-png_seeklogo-394590.png",
-    tags: ["Free", "Education", "Programming", "Beginner"]
+    tags: ["Free", "Education", "Programming", "Beginner"],
   },
   {
     index: "Udemy",
     Website: "https://www.udemy.com",
-    Description: "Udemy is an online learning platform that offers thousands of video courses on a wide variety of topics. It's known for affordable, one-time payment courses taught by industry professionals and instructors from around the world, HTML, CSS, JavaScript, React, Node.js, Python, Java, C++, Rust, Go, Flutter, Android, iOS, Data Science, Machine Learning, AI, Cybersecurity, Ethical Hacking, Unity, Unreal Engine",
+    Description:
+      "Udemy is an online learning platform that offers thousands of video courses on a wide variety of topics. It's known for affordable, one-time payment courses taught by industry professionals and instructors from around the world, HTML, CSS, JavaScript, React, Node.js, Python, Java, C++, Rust, Go, Flutter, Android, iOS, Data Science, Machine Learning, AI, Cybersecurity, Ethical Hacking, Unity, Unreal Engine",
     img: "https://play-lh.googleusercontent.com/dsCkmJE2Fa8IjyXERAcwc5YeQ8_NvbZ4_OI8LgqyjILpXUfS5YhEcnAMajKPrZI-og",
-    tags: ["Paid", "Video Tutorials", "Diverse Courses", "Professional"]
+    tags: ["Paid", "Video Tutorials", "Diverse Courses", "Professional"],
+  },
+  {
+    index: "GameDev.tv",
+    Website: "https://www.gamedev.tv/",
+    Description:
+      "GameDev.tv offers high-quality courses (often via Udemy) focused on game development. Courses go deep into game engines like: Unity, Unreal Engine, C#, C++, Blender (3D modeling), Game Design, Visual Studio, Godot, Blender",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2uOwbIFLXfremVS1VUGYVp--IEOm0ODq4Cg&s",
+    tags: ["Free", "Paid", "Coding Challenges", "Practice", "Advanced"],
   },
   {
     index: "W3 Schools",
     Website: "https://www.w3schools.com",
-    Description: "W3Schools is a popular and beginner-friendly website for learning web development and programming. It provides simple, easy-to-understand tutorials with examples that you can try directly in your browser using its built-in code editor.Popular learning paths: Web Development(HTML, CSS, JavaScript, Bootstrap), Frontend & Backend Technologies(React.js, Angular.js, Node.js, Express.js, PHP, ASP.NET, Java, SQL, MySQL), Python, Java, C++, C#, JSON, AJAX, API, Git, GitHub, Command Line ",
+    Description:
+      "W3Schools is a popular and beginner-friendly website for learning web development and programming. It provides simple, easy-to-understand tutorials with examples that you can try directly in your browser using its built-in code editor.Popular learning paths: Web Development(HTML, CSS, JavaScript, Bootstrap), Frontend & Backend Technologies(React.js, Angular.js, Node.js, Express.js, PHP, ASP.NET, Java, SQL, MySQL), Python, Java, C++, C#, JSON, AJAX, API, Git, GitHub, Command Line ",
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq_fjRVJLHlGB9w7R5zuvuciwZ9zwgGN6-oQ&s",
-    tags: ["Web Development", "Tutorials", "Reference", "Free"]
+    tags: ["Web Development", "Tutorials", "Reference", "Free"],
   },
   {
     index: "GeeksforGeeks",
     Website: "https://www.geeksforgeeks.org",
-    Description: "GeeksforGeeks (GFG) is a widely-used online platform for learning computer science, programming, and technical interview preparation. It’s popular among university students, coding enthusiasts, and job seekers in tech.(Data Structures, Algorithms, Programming Languages, Object-Oriented Programming, Practice for contests like Codeforces, LeetCode, etc.)",
+    Description:
+      "GeeksforGeeks (GFG) is a widely-used online platform for learning computer science, programming, and technical interview preparation. It’s popular among university students, coding enthusiasts, and job seekers in tech.(Data Structures, Algorithms, Programming Languages, Object-Oriented Programming, Practice for contests like Codeforces, LeetCode, etc.)",
     img: "https://geeksforgeeks.zohorecruit.in/recruit/viewCareerImage.do?page_id=61093000000211152&type=logo&file_name=GG_Logo.png",
-    tags: ["Coding Practice", "Interview Prep", "Algorithms", "Data Structures"]
+    tags: [
+      "Coding Practice",
+      "Interview Prep",
+      "Algorithms",
+      "Data Structures",
+    ],
   },
   {
     index: "MDN Web Docs",
     Website: "https://developer.mozilla.org",
-    Description: "MDN Web Docs is a comprehensive resource for web developers, offering documentation and tutorials for HTML, CSS, and JavaScript, Web Development.",
+    Description:
+      "MDN Web Docs is a comprehensive resource for web developers, offering documentation and tutorials for HTML, CSS, and JavaScript, Web Development.",
     img: "https://cdn.neowin.com/news/images/uploaded/2022/03/1648072109_mdn_story.jpg",
-    tags: ["Documentation", "Web Development", "Reference", "Professional"]
+    tags: ["Documentation", "Web Development", "Reference", "Professional"],
   },
   {
     index: "HackerRank",
     Website: "https://www.hackerrank.com",
-    Description: "HackerRank offers coding challenges and competitions to improve programming skills and prepare for technical interviews, Solve problems in many programming languages (Python, Java, C++, JavaScript, etc.).",
+    Description:
+      "HackerRank offers coding challenges and competitions to improve programming skills and prepare for technical interviews, Solve problems in many programming languages (Python, Java, C++, JavaScript, etc.).",
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToJUggt1jqA1FFMwuD0-Vtcx5Qo29hKkBD8g&s",
-    tags: ["Coding Challenges", "Interview Prep", "Programming", "Practice"]
+    tags: ["Coding Challenges", "Interview Prep", "Programming", "Practice"],
   },
   {
     index: "Coursera",
     Website: "https://www.coursera.org",
-    Description: "Coursera partners with universities and companies to offer programming courses, specializations, and certificates, Programming (Python, Java, C++), Web Development (HTML, CSS, JavaScript, React, etc.), Data Structures & Algorithms, Machine Learning & Artificial Intelligence, Cybersecurity, Blockchain, Cloud Computing, Computer Architecture, Operating Systems.",
+    Description:
+      "Coursera partners with universities and companies to offer programming courses, specializations, and certificates, Programming (Python, Java, C++), Web Development (HTML, CSS, JavaScript, React, etc.), Data Structures & Algorithms, Machine Learning & Artificial Intelligence, Cybersecurity, Blockchain, Cloud Computing, Computer Architecture, Operating Systems.",
     img: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://coursera.s3.amazonaws.com/media/coursera-rebrand-logo-square.png?auto=format%2Ccompress&dpr=1",
-    tags: ["University Courses", "Certification", "Professional", "Specialization"]
+    tags: [
+      "University Courses",
+      "Certification",
+      "Professional",
+      "Specialization",
+    ],
   },
   {
     index: "edX",
     Website: "https://www.edx.org",
-    Description: "edX is an online learning platform that provides high-quality programming courses from top universities and organizations.",
+    Description:
+      "edX is an online learning platform that provides high-quality programming courses from top universities and organizations.",
     img: "https://yt3.googleusercontent.com/wtghMyEvmRZ9OSYmFjCN2Hk_4NsENwd6Kmau6k7n0in73FBvyEWpu6W4jDn8MWstLDh11FE8=s900-c-k-c0x00ffffff-no-rj",
-    tags: ["University Courses", "Professional", "Certification", "Advanced"]
-  }
+    tags: ["University Courses", "Professional", "Certification", "Advanced"],
+  },
+  {
+    index: "The Odin Project",
+    Website: "https://www.theodinproject.com/",
+    Description:
+      "A full-stack web development curriculum designed for self-learners, Teaches: HTML, CSS, JavaScript, Git, Node.js, Express, MongoDB, React, GitHub, VS Code, Chrome DevTools.",
+    img: "https://avatars.githubusercontent.com/u/4441966?s=280&v=4",
+    tags: ["Free", "Web Development", "Full Stack", "Practice", "RoadMap"],
+  },
+  {
+    index: "MIT OpenCourseWare (OCW)",
+    Website: "https://ocw.mit.edu/",
+    Description:
+      "MIT OpenCourseWare is a vast library of university-level course content from the Massachusetts Institute of Technology, provided for free (Computer Science, Algorithms, Data Structures, AI, Systems Programming, C, Python, MATLAB, Linux).",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDgYRCMdx5d2TphUD--5PoMRO6N_-Z_yO1RQ&s",
+    tags: ["Free", "Education", "Programming"],
+  },
+  {
+    index: "CS50 – Harvard",
+    Website: "https://cs50.harvard.edu/",
+    Description:
+      "CS50 is Harvard University’s famous introductory course in computer science, offered for free and accessible globally(C, Python, SQL, Web (HTML, CSS, JS), Git, Flask, VS Code (online), GitHub, CLI tools).",
+    img: "https://camo.githubusercontent.com/3206f3affaa684c377e14621f78f9746ba4b0c9054e44fce2868d9ceeb2f30f4/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f7468756d622f632f63632f486172766172645f556e69766572736974795f636f61745f6f665f61726d732e7376672f38303070782d486172766172645f556e69766572736974795f636f61745f6f665f61726d732e7376672e706e67",
+    tags: ["Free", "Web Development", "Programming", "Certification"],
+  },
+  {
+    index: "LeetCode",
+    Website: "https://leetcode.com/",
+    Description:
+      "Platform for practicing coding problems often used in tech job interviews(Problem Solving, Data Structures, Algorithms,  Java, C++, Python, JavaScript, and others).",
+    img: "https://upload.wikimedia.org/wikipedia/commons/1/19/LeetCode_logo_black.png",
+    tags: ["Free", "Paid", "Coding Challenges", "Practice", "Advanced"],
+  },
+  {
+    index: "Replit",
+    Website: "https://leetcode.com/",
+    Description:
+      "Interactive coding lessons and projects that run directly in your browser(Python, JavaScript, HTML/CSS, Bash, Java, C++, Game development with Pygame or Kaboom.js, Data structures, Algorithms, Loops, Functions).",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0dsqNI0yHXvxVpnYE_tyvIpHJP-nMDPu--A&s",
+    tags: ["Paid", "Video Tutorials", "Programming", "Practice"],
+  },
+  {
+    index: "Udacity",
+    Website: "https://www.udacity.com/",
+    Description:
+      "Udacity is an American global for-profit massive open online course provider. It offers vocational courses for professionals, focusing on areas like programming, data science, artificial intelligence, and cloud computing. Courses are structured as Nanodegree programs, which include real-world projects and mentorship.",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7ILyOS_TipESoSlvHVdLhbFGYbq0oyPcT3A&s",
+    tags: ["Paid", "Web Development", "Full Stack", "Video Tutorials"],
+  },
+  {
+    index: "Exercism",
+    Website: "https://exercism.org/",
+    Description:
+      "Exercism is a free and open-source platform that helps learners practice programming through hands-on coding exercises in over 70 programming languages(Python, JavaScript, Java, C++, C#, Go, Ruby, Rust, Swift, TypeScript,Haskell, Elixir, Erlang, OCaml, F#,C, Assembly, Zig, Nim,Bash, Perl, Lua).",
+    img: "https://pbs.twimg.com/profile_images/1433157029707800580/wCOutmH3_400x400.jpg",
+    tags: ["Coding Challenges", "Free", "Practice", "Programming"],
+  },
+  {
+    index: "CareerFoundry",
+    Website: "https://careerfoundry.com/",
+    Description:
+      "A 5-day mini-bootcamp introducing HTML, CSS, JavaScript, and responsive design through project-based learning. It serves as a foundation for further courses in frontend and full-stack development.",
+    img: "https://yt3.googleusercontent.com/xMXJ-3uPs4i_5FICrj68RQsaPqMV6Bzr_12HALRoKaDsMS67q9r-NKMB6zSegmUNMrdGzMjdLXI=s900-c-k-c0x00ffffff-no-rj",
+    tags: ["Free", "Web Development", "Education", "RoadMap"],
+  },
+  {
+    index: "CodeOp",
+    Website: "https://codeop.tech/",
+    Description:
+      "An international coding school offering full-stack development bootcamps, Courses cover JavaScript, React, Node.js, and more(Full Stack Development Bootcamp, Data Science & AI Bootcamp, Data Analytics Bootcamp).",
+    img: "https://shecancode.io/wp-content/uploads/2023/11/CodeOp-768x768.jpg",
+    tags: ["Paid", "Web Development", "Full Stack", "Education"],
+  },
+  {
+    index: "UpSkill",
+    Website: "https://upskillcourses.com/",
+    Description:
+      "Provides a free full-stack web development curriculum covering HTML, CSS, JavaScript, Ruby on Rails, Git, and more, aimed at beginners seeking a comprehensive learning path.",
+    img: "https://top10codingbootcamps.com/wp-content/uploads/2020/09/Screen-Shot-2020-09-27-at-11.52.18.png",
+    tags: ["Free", "Web Development", "Full Stack", "Education"],
+  },
+  {
+    index: "MongoDB University",
+    Website: "https://learn.mongodb.com/",
+    Description:
+      "Provides free courses on MongoDB, covering topics like data modeling, aggregation, and performance, suitable for developers and database administrators seeking to deepen their knowledge.",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2ZYtHv2OLXmthRPbkmENZRXuqBVDwlsrZ1A&s",
+    tags: [
+      "Free",
+      "Data Structures",
+      "Education",
+      "Certification",
+      "Video Tutorials",
+    ],
+  },
+  {
+    index: "Google’s Python Class",
+    Website: "https://developers.google.com/edu/python",
+    Description:
+      "A free class for people with a little bit of programming experience who want to learn Python, including written materials, lecture videos, and lots of code exercises.",
+    img: "https://pathrise-website-guide-wp.s3.us-west-1.amazonaws.com/guides/wp-content/uploads/2020/04/24155859/6w6owI4-_400x400.jpg",
+    tags: ["Free", "Programming", "Education", "Video Tutorials"],
+  },
+  {
+    index: "Sololearn",
+    Website: "https://www.sololearn.com/en/",
+    Description:
+      "Sololearn offers bite-sized lessons in various programming languages, including Python, JavaScript, C++, and Java. Its mobile-friendly platform is great for learning on the go, with interactive quizzes and a supportive community.",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkD7GIyDBboQMD1sAojuRSwmjcAi_9dODhNg&s",
+    tags: ["Free", "Mobile Learning", "Programming", "Practice"],
+  },
 ];
-
-/* test push */
